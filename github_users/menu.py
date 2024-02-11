fc46f75e69fbab2c89a1839e6df114316f026b13
somethingwithspice <email@email.com>
2024-02-11 03:12:43 +0300

    Initial Commit

---
+++ github_users/menu.py
@@ -0,0 +1,38 @@
+import github_users.style
+
+def main_menu():
+    options = 3 # Make sure to change this appropriately
+
+    print(f"""{github_users.style.style.yellow}
+     \_______/
+ `.,-'\_____/`-.,'      ______   _______  ____  ____  ______   ________  _______     
+  /`..'\ _ /`.,'\     .' ____ \ |_   __ \|_  _||_  _||_   _ `.|_   __  ||_   __ \\
+ /  /`.,' `.,'\  \    | (___ \_|  | |__) | \ \  / /    | | `. \ | |_ \_|  | |__) |
+/__/__/     \__\__\__  _.____`.   |  ___/   \ \/ /     | |  | | |  _| _   |  __ /
+\  \  \     /  /  /   | \____) | _| |_      _|  |_    _| |_.' /_| |__/ | _| |  \ \_ 
+ \  \,'`._,'`./  /     \______.'|_____|    |______|  |______.'|________||____| |___|          
+  \,'`./___\,'`./
+ ,'`-./_____\,-'`.
+     /       \{github_users.style.style.end}""")
+    
+    print("\n")
+    print(f"\t{github_users.style.style.red}[1]{github_users.style.style.end} Accumulate GitHub Accounts")
+    print(f"\t{github_users.style.style.red}[2]{github_users.style.style.end} Scan GitHub Accounts")
+    print(f"\t{github_users.style.style.red}[3]{github_users.style.style.end} Get Help")
+    print()
+    while True:
+        user_input = input("Select: ")
+        try:
+            number = int(user_input)
+            if number <= 0 or number > 3:
+                print(f"{github_users.style.style.red}Input must be between 1 and {options}{github_users.style.style.end}")    
+            else:
+                return number
+        except ValueError as value:
+            if user_input.upper() in ["EXIT", "EX", "QUIT", "Q", "END"]:
+                print("...\n[exited]")
+                return -1
+            
+            print(f"{github_users.style.style.red}{value} Input must be a number OR in ['exit', 'ex', 'quit', 'q', 'end']{github_users.style.style.end}")
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import github_users.menu
+
+if __name__ == "__main__":
+    github_users.menu.main_menu()
+
